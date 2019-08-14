a18c2c965908664dc590f9925b3520bdf1240126
Nithin Pingili <nithinp300@gmail.com>
2019-08-14 18:42:26 -0500

    processed the model and checked the prediction with the test and saved the model

--- model.py
+++ model.py
@@ -22,3 +22,35 @@ y = data.quality
 X = data.drop('quality', axis=1)
 # we split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
+
+# we preprocess the data
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+print(X_train_scaled.mean(axis=0))
+print(X_train_scaled.std(axis=0))
+
+X_test_scaled = scaler.transform(X_test)
+print(X_test_scaled.mean(axis=0))
+print(X_test_scaled.std(axis=0))
+
+pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestRegressor(n_estimators=100))
+
+# we declare the hyperparameters
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestregressor__max_depth': [None, 5, 3, 1]}
+
+# we use a cross-validation for the model
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+# we fit and tune the model
+clf.fit(X_train, y_train)
+
+# we evaluate the model pipeline on test data
+y_prediction = clf.predict(X_test)
+print(r2_score(y_test, y_prediction))
+print(mean_squared_error(y_test, y_prediction))
+
+# we save the model to a .pkl file
+joblib.dump(clf, 'rf_model.pkl')
+
+# if we want to load the model we use:
+# clf2 = joblib.load('rf_model.pkl')
